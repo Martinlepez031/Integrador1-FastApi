4708d643a47fbef47372c1bbe6c6d4eaf92ebb8b
Martin <martinlepez031@gmail.com>
2026-05-13 12:08:59 -0300

    main y ventas

--- integrador1/app/main.py
+++ integrador1/app/main.py
@@ -5,10 +5,18 @@ from app.modules.ventas.routers import router as ventas_router
 
 def create_app() -> FastAPI:
     app = FastAPI(
-        title="API Integradora Lepez Martin - Unidad 1",
+        title="API Integrador Lepez Martin - Unidad 1",
         description="Conceptos: Path, Query, Body, Pydantic, Errores.",
         version="1.0.0"
     )
+
+    @app.get("/")
+    def home():
+        return {
+            "message": "API Integrador funcionando correctamente",
+            "docs": "/docs",
+            "redoc": "/redoc"
+        }
     
     app.include_router(producto_router)
     app.include_router(categoria_router)
